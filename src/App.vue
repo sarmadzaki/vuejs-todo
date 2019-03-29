d90a0d4f1d58426cdb62ae1462508beb8ccff8ef
sarmad zaki <sarmad.zaki@yahoo.com>
2019-03-29 12:51:29 +0500

    editing feature added

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,9 @@ export default {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+  font-family: 'Lato', sans-serif;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <input type="search" class="todo-input" 
-        @keyup.enter="addTodo" 
-        placeholder="what are you up to today?" 
-        v-model="newTodo">
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
-        <div>
-        {{ todo.title }}
+    <input
+      type="search"
+      class="todo-input"
+      @keyup.enter="addTodo"
+      placeholder="what are you up to today?"
+      v-model="newTodo"
+    >
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <div v-if="!todo.editing" class="todo-item-label" @dblclick="editTodo(todo)">{{ todo.title }}</div>
+        <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)">
+        <div class="remove-item" @click="removeTodo(index)">
+          <img src="../../public/delete.svg" alt height="24px">
         </div>
-        <div class="remove-item">
-            <img src="../../public/delete.svg" alt="" height="24px">
-        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,30 +33,41 @@ export default {
         {
           id: 1,
           title: "complete vue.js todo today",
-          complete: false
+          complete: false,
+          editing: false
         },
         {
           id: 2,
           title: "To have the chicken dinner",
-          complete: false
+          complete: false,
+          editing: false
         }
       ]
     };
   },
   methods: {
-      addTodo() {
-          if (this.newTodo.trim().length == 0) {
-              return false;
-          }
-          this.todos.push({
-              id: this.idForTodo,
-              title: this.newTodo,
-              complete: false
-          }),
-          this.newTodo = '';
-          this.idForTodo++;
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return false;
       }
-  }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        complete: false
+      }),
+        (this.newTodo = "");
+      this.idForTodo++;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+        todo.editing = true;
+    },
+    doneEdit(todo) {
+        todo.editing = false;
+    }
+  },
 };
 </script>
 
@@ -67,16 +82,39 @@ export default {
   }
 }
 .todo-item {
-    margin-bottom: 12px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .remove-item {
-    cursor: pointer;
-    margin-right: 14px;
-    &:hover {
-        color: black;
-    }
+  cursor: pointer;
+  margin-right: 14px;
+  &:hover {
+    color: black;
+  }
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
+
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-family: 'Lato', sans-serif;
+  &:focus {
+      outline: none;
+  }
+
 }
 </style>
