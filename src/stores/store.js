0b2d7f5e10d935e56b63861ba16d88000e12ba40
sarmad zaki <sarmad.zaki@yahoo.com>
2019-04-01 18:52:58 +0500

    added local storage

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import * as MUTAION_TYPES from './mutaion-types';
+import * as MUTAION_TYPES from "./mutaion-types";
+import { getLocalStorage, setLocalStorage } from "../helpers/local-storage";
+import uuid from 'uuid/v1';
 
 Vue.use(Vuex);
 
@@ -8,10 +10,13 @@ export default new Vuex.Store({
   state: {
     todos: [],
     newTodo: "",
-    idForTodo: 0,
+    idForTodo: '0'
   },
   getters: {
     getTodos(state) {
+      if (getLocalStorage('todos') && getLocalStorage('todos').length > 0) {
+        state.todos = getLocalStorage('todos');
+      }
       return state.todos;
     }
   },
@@ -21,38 +26,42 @@ export default new Vuex.Store({
     },
     [MUTAION_TYPES.ADD_TODO](state, payload) {
       state.todos.push({
-        id: state.idForTodo,
+        id: uuid(),
         title: payload,
         complete: false,
-        editing: false,
+        editing: false
       });
-      state.idForTodo++;
+      setLocalStorage('todos', state.todos);
+      state.idForTodo = state.todos.length++;
     },
     [MUTAION_TYPES.REMOVE_TODO](state, payload) {
       this.state.todos.splice(payload, 1);
+      setLocalStorage('todos', state.todos);
     },
     [MUTAION_TYPES.EDIT_TODO](state, payload) {
-        payload.editing = true;
+      payload.editing = true;
+      setLocalStorage('todos', state.todos);
     },
     [MUTAION_TYPES.DONE_EDITING](state, payload) {
-        payload.editing = false;
-    }
+      payload.editing = false;
+      setLocalStorage('todos', state.todos);
+    },
   },
   actions: {
     getTodo({ commit }, payload) {
       commit(MUTAION_TYPES.GET_TODO, payload);
     },
-    addTodo({commit}, payload) {
-        commit(MUTAION_TYPES.ADD_TODO, payload);
+    addTodo({ commit }, payload) {
+      commit(MUTAION_TYPES.ADD_TODO, payload);
     },
-    removeTodo({commit}, payload) {
-        commit(MUTAION_TYPES.REMOVE_TODO, payload);
+    removeTodo({ commit }, payload) {
+      commit(MUTAION_TYPES.REMOVE_TODO, payload);
     },
-    editTodo({commit}, payload) {
-        commit(MUTAION_TYPES.EDIT_TODO, payload);
+    editTodo({ commit }, payload) {
+      commit(MUTAION_TYPES.EDIT_TODO, payload);
     },
-    doneEditing({commit}, payload) {
-        commit(MUTAION_TYPES.DONE_EDITING, payload);
+    doneEditing({ commit }, payload) {
+      commit(MUTAION_TYPES.DONE_EDITING, payload);
     }
   }
 });
